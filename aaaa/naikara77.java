8e90c43b557f458ee598a43355fd8ad689ce55c3
naikara347 <naikara77@gmail.com>
2024-09-17 22:02:50 +0900

    Update naikara77.java

--- aaaa/naikara77.java
+++ aaaa/naikara77.java
@@ -6,9 +6,9 @@ import mindustry.game.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 
-public class ItemBridgeX extends Mod{
+public class naikara77 extends Mod{
 
-    public ItemBridgeX(){
+    public naikara77(){
         Events.on(EventType.ClientLoadEvent.class, e -> Renderer.init());
         Events.run(Trigger.draw, Renderer::draw);
     }
